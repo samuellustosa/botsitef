b72e06aceffa2ff7d007d476c57d60a803702e54
unknown <grupomateus@PISM211-D0001.mateus.dc>
2025-09-25 16:47:34 -0300

    feat: [describe sua nova funcionali

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,7 @@
-// src/index.ts
+// src/api/index.ts
 import { Telegraf } from 'telegraf';
 import { setupBot } from '../core/bot.ts';
 import dotenv from 'dotenv';
-import safeCompare from 'safe-compare';
 
 dotenv.config();
 
@@ -16,19 +15,9 @@ if (!botToken) {
 const bot = new Telegraf(botToken);
 setupBot(bot);
 
-// Configura o bot para usar webhooks
 // A Vercel irá chamar esta função com as atualizações do Telegram
 export default async (req: any, res: any) => {
     try {
-        const { VERCEL_URL, BOT_TOKEN } = process.env;
-        
-        if (!VERCEL_URL || !BOT_TOKEN) {
-            throw new Error('As variáveis de ambiente VERCEL_URL e BOT_TOKEN devem ser configuradas.');
-        }
-
-        // Define a URL do webhook no Telegram, se ainda não estiver configurada
-        await bot.telegram.setWebhook(`${VERCEL_URL}/api/index`);
-
         // Processa a atualização do Telegram
         await bot.handleUpdate(req.body);
         
